7afe8aca5a27702ce8fd9afa629cae4fc4a996ec
EC2 Default User <ec2-user@ip-172-31-28-117.ap-northeast-1.compute.internal>
2023-04-27 04:47:51 +0000

    0427 14:00 Ajax

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 //(API_KEYには、”取得したAPIキーを記述)
-connst API_KEY = "aec412cd28e1bc45783b476953d50643"
+const API_KEY = "aec412cd28e1bc45783b476953d50643"
 
 $(function(){
   $('#btn').on('click',function(){
     //入力された都市名でWebAPIに天気情報をリクエスト
     $.ajax({
-      url:"https://api.openweathermap.org/data/2.5/weather?q=tokyo&units=metric&appid=aec412cd28e1bc45783b476953d50643" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
-      dateType:'jsonp',
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "aec412cd28e1bc45783b476953d5064",
+      dataType : 'jsonp',
     }).done(function (date){
       //通信成功
-      //位置
-      $('#place').text(date.name);
-      //最高気温
-      $('#temp_max').text(date.main.temp_max);
-      //最低気温
-      $('#temp_min').text(date.main.temp_min);
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
       //湿度
-      $('#humidity').text(date.main.humidity);
+      $('#humidity').text(data.main.humidity);
       //風速
-      $('#speed').text(date.wind.speed);
-      //天気
-      $('#weather').text(date.weather[0].main);
-      //天気アイコン
-      $('img').attr("src","http://openweathermap.org/img/w/" + date.weather[0].icon + ".png")
-      $('img').attr("alt",date.weather[0].main);
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
     }).fail(function (date){
       //通信失敗
       alert('通信に失敗しました')
